e39f6bcf3bfefe8b6cc93f82d05dd12a6d9c8033
Hasan Ayan <hasanayan@me.com>
2021-10-07 15:12:51 +0300

    improved tests

--- test/app1/src/App.tsx
+++ test/app1/src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 //@ts-expect-error
 import Button from "app2/Button";
+import styled from "styled-components";
 
 function App() {
   return (
@@ -9,9 +10,14 @@ function App() {
       This is app1
       <React.Suspense fallback="loading">
         <Button />
+        <StyledButton />
       </React.Suspense>
     </div>
   );
 }
+
+const StyledButton = styled(Button)`
+  color: blue;
+`;
 
 export default App;
--- test/app2/src/Button/index.tsx
+++ test/app2/src/Button/index.tsx
@@ -1,3 +1,17 @@
+import { FC } from "react";
+import styled from "styled-components";
 import "./style.scss";
-const Button = () => <button className="funny-button">Hello from app2</button>;
+
+const Button: FC<{ className?: string }> = ({ className = "" }) => (
+  <StyledButton className={"funny-button " + className}>
+    Hello from app2
+  </StyledButton>
+);
 export default Button;
+
+const StyledButton = styled.button`
+  font-family: JeanSunHo;
+  color: red;
+  font-size: 50px;
+  height: 80px;
+`;
